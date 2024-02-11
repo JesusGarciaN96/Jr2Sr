beb0df393e7cc5c72858f7036c825fefae5c6774
Jesus Alejandro Garcia <garciacode96@gmail.com>
2024-02-11 14:33:22 -0600

    Segundo problema de codewars

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -19,4 +19,28 @@ static int sumaMultiplosTresCinco(int limite)
     return resultado < 0 ? 0 : resultado;
 }
 
-Console.WriteLine(sumaMultiplosTresCinco(16));
+/*
+ * Dado dos números enteros (pueden ser positivos o negativos)
+ * se debe encontra la suma de todos los enteros. 
+ * 
+ * Si los dos números son iguales, se debe retornar uno de los
+ * dos valores.
+ */
+static int sumaEnteros(int primero, int segundo)
+{
+    if (primero == segundo)
+        return primero;
+    else
+    {
+        int resultado = 0;
+        int inicio = primero < segundo ? primero : segundo;
+        int limite = segundo > primero ? segundo : primero;
+        for (int indice = inicio; indice <= limite; indice++)
+        {
+            resultado += indice;
+        }
+        return resultado;
+    }
+}
+
+Console.WriteLine(sumaEnteros(-1, 0));
