7dc4e4a51de35a9c7bdbbe25b80bf5fe46f30264
Jesus Alejandro Garcia <garciacode96@gmail.com>
2024-02-11 13:47:40 -0600

    Aparatado para algoritmos de CodeWars

---
+++ CodeWars/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Dado un número "limite" se deben sumar los valores divisibles entre 3 y 5
+ * si el número es divisible entre 3 y 5 entonces solo se debe sumar una vez
+ * por ejemplo: 15 es divisible entre 3 y 5 y debe sumarse 15 una sola vez.
+ * 
+ * En caso de que el resultado sea negativo el resultado debe ser 0.
+ */
+static int sumaMultiplosTresCinco(int limite)
+{
+    int resultado = 0;
+    for (int indice = 1; indice < limite; indice++)
+    {
+        bool unResiduo = indice % 3 == 0 || indice % 5 == 0;
+        if (unResiduo)
+        {
+            resultado += indice;
+        }
+    }
+    return resultado < 0 ? 0 : resultado;
+}
+
+Console.WriteLine(sumaMultiplosTresCinco(16));
