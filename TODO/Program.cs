05eca8164920465c3517f61806df6d41e76b67d4
Jesus Alejandro Garcia <garciacode96@gmail.com>
2024-02-11 15:47:17 -0600

    Se completo el proyecto TODOList

--- TODO/Program.cs
+++ TODO/Program.cs
@@ -1,4 +1,6 @@
-﻿void mostrarMenu()
+﻿List<string> TODOList = new List<string>();
+
+void mostrarMenu()
 {
     try
     {
@@ -28,31 +30,78 @@ void seleccionarOpcion(char opcion)
     {
         case 's':
         case 'S':
-            Console.WriteLine("Mostramos la lista");
+            mostrarItems();
             Console.WriteLine();
             mostrarMenu();
             break;
         case 'a':
         case 'A':
-            Console.WriteLine("Agregamos elemento");
+            agregarItem();
             Console.WriteLine();
             mostrarMenu();
             break;
         case 'r':
         case 'R':
-            Console.WriteLine("Eliminamos elemento");
+            eliminarItem();
             Console.WriteLine();
             mostrarMenu();
             break;
         case 'e':
         case 'E':
-            Environment.Exit(0);
             break;
         default:
             Console.WriteLine("Opción equivocada, intente de nuevo");
             Console.WriteLine();
             mostrarMenu();
             break;
+    }
+}
+
+void mostrarItems()
+{
+    if (TODOList.Count() > 0)
+    {
+
+        foreach (string item in TODOList)
+        {
+            int index = TODOList.IndexOf(item);
+            Console.WriteLine($"{index + 1}.- {item}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("No hay items registrados.");
+    }
+}
+
+void agregarItem()
+{
+    Console.Write("Ingresa el item: ");
+    string item = Console.ReadLine();
+
+    if (item != null && item != "")
+    {
+        TODOList.Add(item);
+    }
+    else
+    {
+        Console.WriteLine("El item no puede ser vacio.");
+    }
+}
+
+void eliminarItem()
+{
+    Console.Write("Ingresa el indice del item: ");
+    int indiceItem = Convert.ToInt32(Console.ReadLine());
+    if (indiceItem > 0)
+    {
+        string itemEncontrado = TODOList.ElementAt(indiceItem - 1);
+        Console.WriteLine($"Item eliminado: {itemEncontrado}");
+        TODOList.RemoveAt(indiceItem - 1);
+    }
+    else
+    {
+        Console.WriteLine("El indice no corresponde a un item registrado.");
     }
 }
 
