435cf43074f4024f6a15bfc5b7b5242b0085c43a
Jesus Alejandro Garcia <garciacode96@gmail.com>
2024-02-13 22:00:19 -0600

    Primera clase con sobrecarga de constructores

---
+++ POO/Program.cs
@@ -0,0 +1,27 @@
+﻿Rectangle SoccerLand = new Rectangle(12.53, 19.42);
+Console.WriteLine($"El área de la cancha es: {SoccerLand.area()} mts");
+
+class Rectangle
+{
+    private double _width;
+    private double _height;
+
+    public Rectangle() { }
+
+    public Rectangle(int width, int height)
+    {
+        this._width = width;
+        this._height = height;
+    }
+
+    public Rectangle(double width, double height)
+    {
+        this._width = width;
+        this._height = height;
+    }
+
+    public double area()
+    {
+        return this._width * this._height;
+    }
+}
