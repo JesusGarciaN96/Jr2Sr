0e2a94f48141a1cdfade15041235aad733dbe271
Jesus Alejandro Garcia <garciacode96@gmail.com>
2024-02-14 22:02:21 -0600

    Sobrecarga de constructores asi mismos con ':this()' y métodos flecha '() => valor;

--- POO/Program.cs
+++ POO/Program.cs
@@ -1,27 +1,28 @@
 ﻿Rectangle SoccerLand = new Rectangle(12.53, 19.42);
-Console.WriteLine($"El área de la cancha es: {SoccerLand.area()} mts");
+Rectangle Figure = new Rectangle();
+Console.WriteLine($"El área de la cancha es: {SoccerLand.CalculateArea()} mts");
+Console.WriteLine($"El área default es: {Figure.CalculateArea()}");
 
 class Rectangle
 {
     private double _width;
     private double _height;
 
-    public Rectangle() { }
+	// this() hace refencia a si mismo y depende los parámetros que pase
+	// hace referencia a la sobrecarga del constructor correspondiente.
+    public Rectangle() : this(12, 32) { }
 
     public Rectangle(int width, int height)
     {
-        this._width = width;
-        this._height = height;
+        _width = width;
+        _height = height;
     }
 
     public Rectangle(double width, double height)
     {
-        this._width = width;
-        this._height = height;
+        _width = width;
+        _height = height;
     }
 
-    public double area()
-    {
-        return this._width * this._height;
-    }
+    public double CalculateArea() => _width * _height;
 }
